d062a29e50cdbf9957ad9e91e60f40941b848f4b
huanglun <492593760@qq.com>
2017-10-20 15:23:54 +0800

    commit

--- flasky.py
+++ flasky.py
@@ -1,11 +1,18 @@
-from flask import Flask
+from flask.ext.bootstrap import Bootstrap
+from flask import Flask, render_template
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    return render_template('index.html')
+
+
+@app.route('/user/<name>')
+def uesr(name):
+    return  render_template('user.html', name=name)
 
 
 if __name__ == '__main__':
